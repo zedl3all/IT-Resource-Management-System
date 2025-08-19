5789c0648521072cb9498bbd003361017d6c3820
zedl3all <sanguanwongtanapat@gmail.com>
2025-08-19 19:59:59 +0700

    Fix query strings in Room_Model and ensure consistent naming for room properties

--- models/Room_Model.js
+++ models/Room_Model.js
@@ -10,19 +10,49 @@ const Room = {
         });
     },
     getById: (id, callback) => {
-        const query = 'SELECT * FROM rooms WHERE id = ? AND is_deleted = 0'
+        const query = 'SELECT * FROM rooms WHERE room_id = ? AND is_deleted = 0'
         db.query(query, [id], (err, results) => {
             if (err) return callback(err);
             callback(null, results[0]);
         });
     },
     searchByKeyword: (keyword, callback) => {
-        const query = 'SELECT * FROM rooms WHERE name LIKE ? AND is_deleted = 0'
+        const query = 'SELECT * FROM rooms WHERE room_name LIKE ? AND is_deleted = 0'
         db.query(query, [`%${keyword}%`], (err, results) => {
             if (err) return callback(err);
             callback(null, results);
         });
-    }
+    },
     // Add more CRUD operations as needed
+    create: (roomData, callback) => {
+        const query = 'INSERT INTO rooms (room_id, room_name, description, capacity) VALUES (?, ?, ?, ?)';
+        const values = [uuidv4(), roomData.name, roomData.description, roomData.capacity];
+        db.query(query, values, (err, results) => {
+            if (err) return callback(err);
+            callback(null, results.insertId);
+        });
+    },
+    update: (id, roomData, callback) => {
+        const query = 'UPDATE rooms SET room_name = ?, description = ?, capacity = ? WHERE room_id = ? AND is_deleted = 0';
+        const values = [roomData.name, roomData.description, roomData.capacity, id];
+        db.query(query, values, (err, results) => {
+            if (err) return callback(err);
+            callback(null, results);
+        });
+    },
+    soft_delete: (id, callback) => {
+        const query = 'UPDATE rooms SET is_deleted = 1 WHERE room_id = ?';
+        db.query(query, [id], (err, results) => {
+            if (err) return callback(err);
+            callback(null, results);
+        });
+    },
+    restore: (id, callback) => {
+        const query = 'UPDATE rooms SET is_deleted = 0 WHERE room_id = ?';
+        db.query(query, [id], (err, results) => {
+            if (err) return callback(err);
+            callback(null, results);
+        });
+    }
 }
 module.exports = Room;
