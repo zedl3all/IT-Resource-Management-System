64a5dd8e139a842cd27c7bcb2a89fcd981b138da
zedl3all <sanguanwongtanapat@gmail.com>
2025-08-19 23:41:34 +0700

    Add Equipment routes for managing equipment resources

---
+++ routes/EquipmentRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const EquipmentController = require('../controllers/Equipment_Controller');
+
+router.get('/', EquipmentController.getAllEquipment);
+router.get('/:id', EquipmentController.getEquipmentById);
+router.get('/search', EquipmentController.searchEquipment);
+router.post('/', EquipmentController.createEquipment);
+router.put('/:id', EquipmentController.updateEquipment);
+router.delete('/:id', EquipmentController.softDeleteEquipment);
+router.post('/:id/restore', EquipmentController.restoreEquipment);
+
+module.exports = router;
