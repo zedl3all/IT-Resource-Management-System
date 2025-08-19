5a4a946eb78de9c8a2e9bf40b60283bad3f2a801
zedl3all <sanguanwongtanapat@gmail.com>
2025-08-19 20:08:40 +0700

    Implement RoomRoute for managing room-related API endpoints

---
+++ routes/RoomRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const RoomController = require('../controllers/Room_Controller');
+
+router.get('/', RoomController.getAllRooms);
+router.get('/:id', RoomController.getRoomById);
+router.get('/search', RoomController.searchRooms);
+router.post('/', RoomController.createRoom);
+router.put('/:id', RoomController.updateRoom);
+router.delete('/:id', RoomController.softDeleteRoom);
+router.post('/:id/restore', RoomController.restoreRoom);
+
+module.exports = router;
