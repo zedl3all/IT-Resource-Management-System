2b3baf9bbb2196eba1ae2ab12c64ad6dc98ce3a6
zedl3all <sanguanwongtanapat@gmail.com>
2025-09-09 14:41:55 +0700

    Update registration form and styles; add bcrypt dependency

--- routes/WebRoutes.js
+++ routes/WebRoutes.js
@@ -4,17 +4,10 @@ const router = express.Router();
 const UserController = require('../controllers/User_Controller');
 router.get('/', UserController.getUserView);
 
-const EquipmentController = require('../controllers/Equipment_Controller');
-
 router.get('/register', (req, res) => {
-    res.render('register/register', {
-        pageTitle: 'เพิ่มอุปกรณ์',
-        heading: 'ฟอร์มเพิ่มอุปกรณ์',
-        assetTypes: ['Laptop', 'Printer', 'Projector'],
-        statuses: ['พร้อมใช้งาน', 'ซ่อมบำรุง', 'ชำรุด']
-    });
+    res.render('register');
 });
 
-router.post('/add-asset', EquipmentController.createEquipment);
+
 
 module.exports = router;
