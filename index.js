abafbc723ff47d5d2f5f2ed9c4d47f2c8bf32df4
zedl3all <sanguanwongtanapat@gmail.com>
2025-09-17 20:41:18 +0700

    Add cookie-parser middleware and update dependencies

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 3000;
 const path = require('path');
 
 // Middleware
 app.use(express.json());
+app.use(cookieParser());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
