093b6d143239eeb531ac029e9b5375d8ab95c583
zedl3all <sanguanwongtanapat@gmail.com>
2025-09-19 18:39:37 +0700

    Refactor Web_Controller: rename repair variable to maintenance and simplify data structure in getStaffView method

--- controllers/Web_Controller.js
+++ controllers/Web_Controller.js
@@ -1,5 +1,6 @@
 const Room = require('../models/Room_Model');
-const repair = require('../models/maintenance_Model');
+const maintenance = require('../models/maintenance_Model');
+const Equipment = require('../models/Equipment_Model');
 
 const WebController = {
     getStaffView: (req, res) => {
@@ -7,19 +8,10 @@ const WebController = {
             if (err) {
                 return res.status(500).json({ message: 'Database error', error: err.message });
             }
-
-            repair.getAll((err, repairs) => {
-                if (err) {
-                    return res.status(500).json({ message: 'Database error', error: err.message });
-                }
-
-                const data = {
-                    title: "Staff Page",
-                    rooms: rooms,
-                    repairs: repairs
+            const data = {
+                    rooms: rooms
                 };
                 res.render('staff', { data });
-            });
         });
     }
 };
