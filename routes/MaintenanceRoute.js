632ca8f7093b9d5da289f7806b337a9e501e0d80
FirzKung <66070266@kmitl.ac.th>
2025-09-19 19:23:29 +0700

    add Maintenance Controller/Routes

---
+++ routes/MaintenanceRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const MaintenanceController = require('../controllers/Maintenance_Controller');
+
+router.get('/', MaintenanceController.getAllMaintenances);
+router.get('/:id', MaintenanceController.getMaintenanceById);
+router.get('/search/:keyword', MaintenanceController.searchMaintenances);
+router.post('/', MaintenanceController.createMaintenance);
+router.put('/:id', MaintenanceController.updateMaintenance);
+router.delete('/:id', MaintenanceController.softDeleteMaintenance);
+router.post('/:id/restore', MaintenanceController.restoreMaintenance);
+
+module.exports = router;
